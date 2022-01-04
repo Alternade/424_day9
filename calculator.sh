c2a4b49af96cdd419289c555a0c8b436177bc17b
Alternade <aptsahu2@gmail.com>
2022-01-04 18:20:22 +0530

    Added functions and refactored the code

--- calculator.sh
+++ calculator.sh
@@ -1,13 +1,45 @@
+function sum() {
+	sum=$(($a+$b))
+	echo "Sum is $sum"
+}
+
+function sub() {
+	sub=$(($a-$b))
+	echo "Sub is $sub"
+}
+function mul() {
+	mul=$(($a*$b))
+	echo "Mul is $mul"
+}
+function div() {
+	div=$(($a/$b))
+	echo "Div is $div"
+}
+
 echo "Enter a number"
 read a
 echo "Enter 2nd number"
 read b
-sum=$(($a+$b))
-echo "Sum is $sum"
-sub=$(($a-$b))
-echo "Sub is $sub"
-mul=$(($a*$b))
-echo "Mul is $mul"
-div=$(($a/$b))
-echo "Div is $div"
+echo "Chose 1 - Addition"
+echo "Chose 2 - Substraction"
+echo "Chose 3 - Multiplication"
+echo "Chose 4 - Division"
+read option
+case $option in
+	1)
+		sum
+		;;
+	2)
+		sub
+		;;
+	3)
+		mul
+		;;
+	4)
+		div
+		;;
+	*)
+		echo "Valid option not give"
+		;;
+esac
 
